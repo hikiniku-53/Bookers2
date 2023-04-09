339c0158de5df002064d149fa1dac964279be023
EC2 Default User <ec2-user@ip-172-31-21-240.ap-northeast-1.compute.internal>
2023-04-09 21:12:43 +0000

    SHOW_FINISH_0410

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,34 @@
 class BooksController < ApplicationController
-  
+
   def show
+    @books = Book.all
+    @book = Book.find(params[:id])
+    @users = User.all
+    @user = User.find(@book.user_id)
   end
-  
+
   def index
+    @users = User.all
+    @user = User.find(current_user.id)
+    @books = Book.all
+    @book = Book.new
   end
-  
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(book.id)
+  end
+
   def edit
+
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,33 +2,40 @@ class UsersController < ApplicationController
     before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
-    
+    @users = User.all
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.find(params[:id])
   end
 
   def index
+    @users = User.all
+    @user = User.find(current_user.id)
+    @books = Book.all
+    @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to post_images_path
     end
   end
-  
+
 end
